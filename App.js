13f745a121a5568a08c439a042c07c1867cc0baf
kdc1 <kdc_1@naver.com>
2019-12-18 23:20:29 +0900

    ask permission

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>style={styles.text}Hello!!</Text>
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const{
+        coords: {latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading } = this.state;
+    return isLoading ? <Loading />: null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "white"
-  }
-});
+
+  
+
